b7540fafe2130e5c0c173e572f439827bc501f0b
jorm <scarletemperor@protonmail.com>
2023-05-03 21:16:15 +0200

    playing around with next layouts

---
+++ app/about/layout.tsx
@@ -0,0 +1,7 @@
+export default function Layout() {
+	return (
+		<main className="font-retro">
+			<p>about</p>
+		</main>  
+	)
+}
---
+++ app/posts/layout.tsx
@@ -0,0 +1,7 @@
+export default function Layout() {
+	return (
+		<main className="font-retro">
+			<p>posts</p>
+		</main>  
+	)
+}
